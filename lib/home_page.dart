2ecec759e5d71fe3a91782148f425606c07d4adc
hhy0902 <hhy09020@naver.com>
2024-11-23 01:14:10 +0900

    코드 최적화 및 SpeedDial추가 예정

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_tabbar_exam/provider/firebase_item_provider.dart';
 
 class MyHomePage extends ConsumerStatefulWidget {
@@ -35,7 +36,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with TickerProviderStat
         title: const Text('TabBar Example'),
         actions: [
           TextButton(
-            onPressed: () {
+            onPressed: () async {
               showDialog(
                 context: context,
                 builder: (context) {
@@ -50,24 +51,51 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with TickerProviderStat
                     ),
                     actions: [
                       TextButton(
-                        onPressed: () {
-                          if (textController.text.isNotEmpty) {
-                            db
-                                .collection("category")
-                                .doc(textController.text)
-                                .set({
+                        onPressed: () async {
+                          if (textController.text.isEmpty) {
+                            return;
+                          }
+
+                          final docRef = db.collection("category").doc(textController.text);
+
+                          // 문서 존재 여부 확인
+                          final docSnapshot = await docRef.get();
+
+                          if(docSnapshot.exists) {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text("Duplicate Tab"),
+                                  content: const Text("This tab already exists."),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        textController.clear();
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("OK"),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            await docRef.set({
                               "categoryName": textController.text,
                               "createdAt": FieldValue.serverTimestamp(),
                             });
+                            textController.clear();
+                            Navigator.pop(context);
                           }
-                          textController.clear();
-                          Navigator.pop(context);
+
                         },
                         child: const Text("Add"),
                       ),
                       TextButton(
                         onPressed: () {
                           Navigator.pop(context);
+                          textController.clear();
                         },
                         child: const Text("Cancel"),
                       ),
@@ -123,7 +151,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with TickerProviderStat
                         StreamBuilder<QuerySnapshot>(
                           stream: db
                               .collection("category")
-                              .doc(snapshot.data!.docs[tabList.indexOf(tabTitle)].id)
+                              .doc(currentCategoryValue.value!.docs[currentTabIndex].id)
                               .collection("food")
                               .snapshots(),
                           builder: (context, snapshot) {
@@ -151,6 +179,9 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with TickerProviderStat
                                         print(foodItems[index].id);
                                         print(currentCategoryValue.value!.docs[currentTabIndex].id);
 
+                                        print(currentCategoryValue.value!.docs[currentTabIndex].id);
+                                        print(currentTabIndex);
+
                                       },
                                       icon: Icon(Icons.delete),
                                     ),
@@ -169,68 +200,126 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with TickerProviderStat
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
 
-          final documentId = currentCategoryValue.value!.docs[currentTabIndex].id;
-          print(currentCategoryValue.value!.docs[tabController!.index].id);
-          print('현재 선택된 Tab: ${tabController!.index}');
-          print(currentTabIndex);
+              final documentId = currentCategoryValue.value!.docs[currentTabIndex].id;
+              print(currentCategoryValue.value!.docs[tabController!.index].id);
+              print('현재 선택된 Tab: ${tabController!.index}');
+              print(currentTabIndex);
+              print(documentId);
 
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: const Text("음식 입력"),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: textController,
-                      autofocus: true,
-                      decoration: const InputDecoration(
-                        labelText: "음식명",
-                      ),
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("음식 입력"),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          controller: textController,
+                          autofocus: true,
+                          decoration: const InputDecoration(
+                            labelText: "음식명",
+                          ),
+                        ),
+                        TextField(
+                          controller: priceController,
+                          decoration: const InputDecoration(
+                            labelText: "가격",
+                          ),
+                        ),
+                      ],
                     ),
-                    TextField(
-                      controller: priceController,
-                      decoration: const InputDecoration(
-                        labelText: "가격",
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+
+                          if (textController.text.isEmpty || priceController.text.isEmpty) {
+                            return;
+                          }
+
+                          final docRef = db.collection("category").doc(documentId).collection("food").doc(textController.text);
+
+                          final docSnapshot = await docRef.get();
+                          print(docSnapshot.exists);
+
+                          if(docSnapshot.exists) {
+                            showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text("Duplicate item"),
+                                  content: const Text("This item already exists."),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        textController.clear();
+                                        priceController.clear();
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text("OK"),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            await docRef.set({
+                              "제품 명": textController.text,
+                              "가격": priceController.text,
+                            });
+
+                            textController.clear();
+                            priceController.clear();
+                            Navigator.pop(context);
+                          }
+
+                        },
+                        child: const Text("입력"),
                       ),
-                    ),
-                  ],
-                ),
-                actions: [
-                  TextButton(
-                    onPressed: () {
-                      db
-                          .collection("category")
-                          .doc(documentId)
-                          .collection("food")
-                          .doc(textController.text)
-                          .set({
-                        "제품 명": textController.text,
-                        "가격": priceController.text,
-                      });
-                      textController.clear();
-                      priceController.clear();
-                      Navigator.pop(context);
-                    },
-                    child: const Text("입력"),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text("취소"),
-                  ),
-                ],
+                      TextButton(
+                        onPressed: () {
+                          textController.clear();
+                          priceController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text("취소"),
+                      ),
+                    ],
+                  );
+                },
               );
-            },
-          );
 
-        },
-        child: const Icon(Icons.add),
+            },
+            child: const Icon(Icons.edit),
+          ),
+          SpeedDial(
+            animatedIcon: AnimatedIcons.menu_close,
+            overlayOpacity: 0.5,
+            children: [
+              SpeedDialChild(
+                child: const Icon(Icons.add),
+                label: 'Add Item',
+                onTap: () => print('Add Item tapped'),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.edit),
+                label: 'Edit Item',
+                onTap: () => print('Edit Item tapped'),
+              ),
+              SpeedDialChild(
+                child: const Icon(Icons.delete),
+                label: 'Delete Item',
+                onTap: () => print('Delete Item tapped'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
